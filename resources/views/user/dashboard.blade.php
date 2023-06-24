30486d7c9e436f663db4ac03b5de1231cadd5ebc
Hidayaayt09 <hidayaayt09@gmail.com>
2023-06-24 14:39:51 +0700

    commit all midtrans

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -35,13 +35,13 @@
                                 <strong>{{$checkout->camp->price}}k</strong>
                             </td>
                             <td>
-                               @if ($checkout->is_paid)
+                                <strong>{{$checkout->payment_status}}</strong>
+                            </td>
+                            
+                            <td>
+                               @if ($checkout->payment_status == 'waiting')
 
-                               <strong class="text-success">Payment Success</strong>
-
-                               @else
-
-                               <strong>Waiting for Payment</strong>
+                               <a href="{{$checkout->midtrans_url}}" class="btn btn-primary">Pay Here</a>
 
                                @endif
                             </td>
@@ -55,7 +55,7 @@
                         @empty
                             <tr> 
                                 <td colspan="5">
-                                <h3>No Data</h3>
+                                <h3>No Camp Registered</h3>
                             </td>
                             </tr>
 
